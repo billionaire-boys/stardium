776f14489767365a9535e1af0caa44ccded3075c
Jo Jinsik <kunggom@gmail.com>
2019-11-28 17:56:00 +0900

    feat(Player): toString(), equals(), hash() 추가

--- src/main/java/com/bb/stardium/player/domain/Player.java
+++ src/main/java/com/bb/stardium/player/domain/Player.java
@@ -1,5 +1,7 @@
 package com.bb.stardium.player.domain;
 
+import java.util.Objects;
+
 public class Player {
     private String nickname;
     private String email;
@@ -28,5 +30,26 @@ public class Player {
         this.email = newPlayer.email;
         this.password = newPlayer.password;
         return this;
+    }
+
+    @Override
+    public boolean equals(final Object another) {
+        if (this == another) return true;
+        if (another == null || getClass() != another.getClass()) return false;
+        final Player player = (Player) another;
+        return Objects.equals(email, player.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+
+    @Override
+    public String toString() {
+        return "Player {" +
+                "nickname: \"" + nickname + "\"" +
+                ", email: \"" + email + "\"" +
+                "}";
     }
 }
