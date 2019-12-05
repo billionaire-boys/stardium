cd138cf58816c551d0f3e6138c1d385152d06b37
jinwook <jin942002@naver.com>
2019-12-05 13:53:30 +0900

    refactor: 방 만들고 퇴장하는 acceptance 테스트 수정
    json contentype으로 통신하도록 수정

--- src/test/java/com/bb/stardium/acceptance/RoomTest.java
+++ src/test/java/com/bb/stardium/acceptance/RoomTest.java
@@ -6,6 +6,7 @@ import com.bb.stardium.player.dto.PlayerRequestDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
@@ -18,28 +19,34 @@ public class RoomTest extends BaseAcceptanceTest {
     void setUp() {
         roomRequestDto = new RoomRequestDto("title", "intro",
                 new Address("서울시", "송파구", "루터회관"),
-                LocalDateTime.now(), LocalDateTime.now().plusHours(1L), 3);
+                LocalDateTime.now().plusHours(2L), LocalDateTime.now().plusHours(3L), 3);
     }
 
     @Test
     @DisplayName("사용자가 방을 만들고 들어가고 나올 수 있다")
     void joinRoom() {
-        PlayerRequestDto createPlayer = new PlayerRequestDto("test", "create@room.com", "A!1bcdefg", "dd");
-        String roomUri = newSessionPost(createPlayer, "/rooms/new")
+        PlayerRequestDto createPlayer = new PlayerRequestDto("test", "create@room.com", "A!1bcdefg", "200");
+        Long roomNumber = newSessionPost(createPlayer, "/rooms")
+                .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(roomRequestDto), RoomRequestDto.class)
                 .exchange()
                 .expectStatus()
-                .is3xxRedirection()
-                .returnResult(String.class).getRequestHeaders().getFirst("Location");
+                .is2xxSuccessful()
+                .expectBody(Long.class)
+                .returnResult()
+                .getResponseBody();
+
 
         PlayerRequestDto joinPlayer = new PlayerRequestDto("join", "join@room.com", "A!1bcdefg", "dd");
-        newSessionPost(joinPlayer, roomUri)
+        newSessionPost(joinPlayer, "rooms/join/" + roomNumber)
+                .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(joinPlayer), PlayerRequestDto.class)
                 .exchange()
                 .expectStatus()
                 .isOk();
 
-        loginSessionPost(joinPlayer, roomUri + "/quit")
+        loginSessionPost(joinPlayer, "rooms/quit" + roomNumber)
+                .contentType(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus()
                 .is3xxRedirection();
