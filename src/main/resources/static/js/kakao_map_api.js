286acf82b040e97d7df126ad2f6575bd85229fc0
backlo <hjun4512@naver.com>
2019-12-11 10:19:38 +0900

    refactor: Room.html map api 리펙토링

--- src/main/resources/static/js/kakao_map_api.js
+++ src/main/resources/static/js/kakao_map_api.js
@@ -28,35 +28,35 @@ const MAP_API = (() => {
         const searchAddress = (location) => {
             geocoder.addressSearch(location, (result, status) => {
                 if (status === kakao.maps.services.Status.OK) {
-                    statusOK(map, result);
+                    statusOK(map, result, location);
                 } else {
                     statusFailed(map);
                 }
             });
         };
 
-        const statusOK = (map, result) => {
+        const statusOK = (map, result, location) => {
             const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
             const marker = new kakao.maps.Marker({
                 map: map,
                 position: coords,
             });
-            const infowindow = new kakao.maps.InfoWindow({
-                content: '<div class="info_window">위치</div>',
+            const infoWindow = new kakao.maps.InfoWindow({
+                content: `<div class="info_window">${location}</div>`,
             });
 
-            infowindow.open(map, marker);
+            infoWindow.open(map, marker);
             map.setCenter(coords);
         };
 
         const statusFailed = (map) => {
             const centerPosition = new kakao.maps.LatLng(37.515412, 127.103040);
-            const infowindow = new kakao.maps.InfoWindow({
+            const infoWindow = new kakao.maps.InfoWindow({
                 content: '<div class="info_window"> 위치를 찾을 수 없습니다.</div>',
                 position: centerPosition
             });
 
-            infowindow.open(map);
+            infoWindow.open(map);
             map.setCenter(centerPosition);
         };
 
